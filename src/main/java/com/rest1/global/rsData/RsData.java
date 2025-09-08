7e8205ac11be1ad66124e53ed948ee8169463cb8
chacha86 <taejincha0619@gmail.com>
2025-09-08 14:13:43 +0900

    work

--- src/main/java/com/rest1/global/rsData/RsData.java
+++ src/main/java/com/rest1/global/rsData/RsData.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class RsData {
+public class RsData<T> {
 
     private String resultCode;
     private String msg;
-    private Object data;
+    private T data;
 
 }

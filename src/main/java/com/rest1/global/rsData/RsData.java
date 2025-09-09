4499558473fab5b7da5113cb3cdc25876b87bcd4
chacha86 <taejincha0619@gmail.com>
2025-09-09 12:14:09 +0900

    work

--- src/main/java/com/rest1/global/rsData/RsData.java
+++ src/main/java/com/rest1/global/rsData/RsData.java
@@ -1,5 +1,6 @@
 package com.rest1.global.rsData;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -15,6 +16,12 @@ public class RsData<T> {
         this.resultCode = resultCode;
         this.msg = msg;
         this.data = null;
+    }
+
+    @JsonIgnore
+    public int getStatusCode() {
+        String statusCode = resultCode.split("-")[0];
+        return Integer.parseInt(statusCode);
     }
 
 }
